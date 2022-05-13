dd96f69bd688cac8666848e83fbd95f4e8359041
Ítalo <italo.mtr@gmail.com>
2022-05-13 20:37:09 -0300

    tentativa 2

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,6 @@ const mongoose = require('mongoose');
 
 
 const url = process.env.CUSTOMCONNSTR_connectionStringV2 || config.connectionStringV2;
-mongoose.connect(url, {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true}).then(()=> {console.log("conectou com sucesso")}).catch((error)=>{console.log(error)});
 
 module.exports = mongoose;
