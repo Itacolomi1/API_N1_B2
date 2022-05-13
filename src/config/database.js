48998c7fc9bb835cd314dd63e68adc4227731c5b
Ítalo <italo.mtr@gmail.com>
2022-05-13 20:16:49 -0300

    feat: set to prod

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1:27017/ESTOQUE_DB?retryWrites=true&w=majority'
+const url = 'mongodb+srv://userLibras4All:estoque@cluster0.wfd3t.mongodb.net/ESTOQUE_DB?retryWrites=true&w=majority'
 mongoose.connect(url, {useNewUrlParser: true});
 
 
