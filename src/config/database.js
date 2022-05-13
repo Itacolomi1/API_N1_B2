205e67e0f683da67bd2a9108363a23132740b6ea
Ítalo <italo.mtr@gmail.com>
2022-05-13 20:39:38 -0300

    tentativa 3

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
-import * as config from './appSettings.json'
-
-
 const mongoose = require('mongoose');
 
 
-const url = process.env.CUSTOMCONNSTR_connectionStringV2 || config.connectionStringV2;
+const url = process.env.CUSTOMCONNSTR_connectionStringV2 || "string aleatoria";
 mongoose.connect(url, {useNewUrlParser: true}).then(()=> {console.log("conectou com sucesso")}).catch((error)=>{console.log(error)});
 
 module.exports = mongoose;
